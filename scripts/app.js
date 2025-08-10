9bacefcac3053fc287d51528b0727ece2d4d1092
ahmer476 <ahmer873@gmail.com>
2025-08-10 19:48:28 -0400

    adding
    add some code

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            city: 'London',
+            province: 'Ontario',
+            country: 'Canada',
+            word: '',
+        };
+    },
+    methods: {
+        // Add your methods here, e.g.:
+        getWeather() {
+			const url = `https://comp6062.liamstewart.ca/weather-data?city=${this.city}&province=${this.province}&country=${this.country}`;
+    
+			fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					console.log('Weather Data:', data);
+				})
+				.catch(error => {
+					console.error('Error fetching weather data:', error);
+				});
+		}
+
+        defineWord() {
+			const url = `https://comp6062.liamstewart.ca/api/define?word=${this.word}`;
+    
+			fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					console.log('Definition:', data);
+				})
+				.catch(error => {
+					console.error('Error fetching definition:', error);
+				});
+		}
+
+    }
+}).mount('#app');
